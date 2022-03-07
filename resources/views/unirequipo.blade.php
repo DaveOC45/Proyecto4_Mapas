22c3b090c4debb8816bd42d8a3bdf9f0dec7c850
David <6694.joan23@fje.edu>
2022-03-07 15:50:01 +0100

    creacion blades + carpetas css y js

---
+++ resources/views/unirequipo.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Unirse a un equipo</title>
+</head>
+<body>
+    <h1>Unirse a un equipo</h1>
+    
+</body>
+</html>
