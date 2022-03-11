2ad0bec26b1cca0963e65b8e57c4c4c7331b32cf
David <6694.joan23@fje.edu>
2022-03-11 16:03:16 +0100

    inicio cambio

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::post('leertipo',[AdministracionController::class, 'lecturatipoubicacion']
 //Ruta para eliminar ubicación.
 Route::delete('eliminar/{id}', [AdministracionController::class, 'eliminarController']);
 
-//---------- ECOSISTEMA DE LOS MAPAS ------------//
+//---------- ECOSISTEMA DE LOS MAPAS PARTE ALVAREZ------------//
 //Ruta para el login
 Route::get('login', [MapaController::class, 'login']);
 Route::post('loginPost',[MapaController::class, 'loginPost']);
@@ -52,3 +52,55 @@ Route::get('/jugargimcana2', [MapaController::class, 'jugargimcana2']);
 Route::put('modificar',[AdministracionController::class, 'modificarController']);
 // ruta para eliminar ubicación.
 Route::delete('eliminar/{id}', [AdministracionController::class, 'eliminarController']);
+
+
+/*ECOSISTEMA DEL ADMIN ORTEGA*/
+/*UNIRSE UN EQUIPO*/
+Route::get('unirseequipo',[AdministracionController::class, 'unirseequipo']);
+Route::post('unirsePostequipo',[AdministracionController::class, 'unirsePostequipo']);
+/*REGISTRO DE EQUIPO*/
+Route::get('registroequipo',[AdministracionController::class, 'registroequipo']);
+Route::post('registroPostequipo',[AdministracionController::class, 'registroPostequipo']);
+/*LOGIN Y LOGOUT*/
+Route::post('login', [AdministracionController::class, 'loginP']);
+Route::get('logout', [AdministracionController::class, 'logout']);
+
+
+//RUTAS PANEL PRINCIPAL
+Route::get('/', function () {
+    return view('login');
+});
+Route::get('cPanelAdmin', function () {
+    return view('cPanelAdmin');
+});
+Route::get('principal', function () {
+    return view('principal');
+});
+Route::get('usuarios', function () {
+    return view('usuarios');
+});
+
+//AJAX UBICACION
+// ruta para leer ubicación.
+Route::post('leer',[AdministracionController::class, 'leerController']);
+// ruta para insertar/crear ubicación.
+Route::post('crear',[AdministracionController::class, 'crearController']);
+//Esto de aquí lo hicimos para poder hacer el select de tipo de ubicación.
+Route::post('leertipo',[AdministracionController::class, 'lecturatipoubicacion']);
+// ruta para modificar ubicación.
+Route::put('modificar',[AdministracionController::class, 'modificarController']);
+// ruta para eliminar ubicación.
+Route::delete('eliminar/{id}', [AdministracionController::class, 'eliminarController']);
+
+
+//AJAX USUARIOS
+// ruta para leer usuario.
+Route::post('leeruser',[AdministracionController::class, 'leerControlleruser']);
+// ruta para insertar/crear usuario.
+Route::post('crearuser',[AdministracionController::class, 'crearControlleruser']);
+//Esto de aquí lo hicimos para poder hacer el select de tipo de usuario.
+Route::post('leertipouser',[AdministracionController::class, 'lecturatipoubicacionuser']);
+// ruta para modificar
+Route::put('modificaruser',[AdministracionController::class, 'modificarControlleruser']);
+// ruta para eliminar usuario.
+Route::delete('eliminaruser/{id}', [AdministracionController::class, 'eliminarControlleruser']);
